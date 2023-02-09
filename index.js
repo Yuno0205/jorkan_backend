536e7da7fb37b14e8dc88ba2807186adf2db6b59
Yuno <mainhathao195@gmail.com>
2023-02-09 14:47:11 +0700

    Test

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://delightful-pithivier-ab0977.netlify.app",
-    methods: "GET,POST,PUT,DELETE",
+    methods: "GET,POST,PUT,DELETE,FETCH",
     credentials: true,
   })
 );
