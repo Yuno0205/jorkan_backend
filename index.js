bebebf9bcce157618c8964ba9ac8082f25956940
Yuno <mainhathao195@gmail.com>
2023-02-10 12:02:47 +0700

    Commit

--- index.js
+++ index.js
@@ -16,16 +16,18 @@ const passport = require("passport");
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: [
-      "https://delightful-pithivier-ab0977.netlify.app",
-      "https://jorkan-backend.vercel.app",
-    ],
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://delightful-pithivier-ab0977.netlify.app",
+//       "https://jorkan-backend.vercel.app",
+//     ],
+//     methods: "GET,POST,PUT,DELETE",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors({ origin: "*" }));
 
 app.use(morgan("combined"));
 
