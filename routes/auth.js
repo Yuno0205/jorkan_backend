0ba4e39224561450ed1d33e87f5afffc42227fd4
Yuno <mainhathao195@gmail.com>
2023-02-11 17:42:50 +0700

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-const CLIENT_URL = "https://sensational-toffee-a5007d.netlify.app/";
+const CLIENT_URL = "https://sensational-toffee-a5007d.netlify.app";
 
 router.get("/login/success", (req, res) => {
   if (req.user) {
