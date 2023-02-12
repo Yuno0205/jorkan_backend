a29de3856d61640d987e3f12816040c9ef39a1bc
Yuno <mainhathao195@gmail.com>
2023-02-12 14:44:04 +0700

    Fix

--- config/passport.js
+++ config/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://jorkan-backend.vercel.app/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const newUser = {
--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,58 @@
 const router = require("express").Router();
 const passport = require("passport");
-
+const { OAuth2Client } = require("google-auth-library");
 const CLIENT_URL = "https://sensational-toffee-a5007d.netlify.app/";
 
 router.get("/login/success", (req, res) => {
   console.log("Request in success :", req);
-  // if (req?.user) {
-  return res.status(200).json({
-    success: true,
-    message: "Successfull",
-    user: req?.user,
-  });
-  // } else {
-  //   return res.json({
-  //     success: false,
-  //     message: "Failure !",
-  //   });
-  // }
+  if (req.user) {
+    return res.status(200).json({
+      success: true,
+      message: "Successfull",
+      user: req?.user,
+    });
+  } else {
+    return res.status(500).json({
+      success: true,
+      message: "User not found",
+      user: req?.user,
+    });
+  }
 });
+// const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+// router.post("/login/google", async (req, res, next) => {
+//   //const {idToken} = req.body;
+//   console.log("idToken", idToken);
+//   const ticket = await client.verifyIdToken({
+//     idToken: idToken,
+//     audience: process.env.GOOGLE_CLIENT_ID,
+//   });
+//   console.log("ticket nè", ticket);
+//   const { name, family_name, picture, sub, email } = ticket.getPayload();
+//   let user = await User.findOne({ email });
+//   if (!user) {
+//     user = await User.create({
+//       name,
+//       email,
+//       avatar: picture,
+//       googleId: sub,
+//       password: 123,
+//       displayName: toSlug(name),
+//     });
+//   }
+//   const accessToken = await user.generateToken();
 
+// return sendResponse(
+//     res,
+//     200,
+//     true,
+//     { user, accessToken },
+//     null,
+//     "Successfully sign up with google"
+//   );//
+//     "Successfully sign up with google"
+//   );//
+// });
 router.get("/login/failed", (req, res) => {
   res.status(401).json({
     success: false,
